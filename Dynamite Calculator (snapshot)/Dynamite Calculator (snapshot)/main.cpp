27d7ac4cca0731b7ff83af717aa8f4f337e7b471
TermiNateVader <nathanbenstevens@gmail.com>
2021-02-09 15:54:32 -0500

    Add files via upload

---
+++ Dynamite Calculator (snapshot)/Dynamite Calculator (snapshot)/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double a;
+	double b;
+	char op;
+	while (true) {
+		cout << "Dynamite Calculator \n";
+		cout << "snapshot 2021.02.09 \n";
+		cin >> a >> op >> b;
+		while (cin.fail()) {
+			cin.clear(); 
+			cin.ignore(100, '\n'); 
+			
+			{
+				switch (op)
+				{
+				case '+':
+					cout << a + b << endl;
+					break;
+				case '-':
+					cout << a - b << endl;
+					break;
+				case '*':
+					cout << a * b << endl;
+					break;
+				case '/':
+					cout << a / b << endl;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	}
+}
