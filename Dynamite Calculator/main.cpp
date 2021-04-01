a11071b9c0e54d0e1160639db16c9913e2d492bb
TermiNateVader <nathanbenstevens@gmail.com>
2021-03-31 21:10:12 -0400

    Update main.cpp

--- Dynamite Calculator/main.cpp
+++ Dynamite Calculator/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdio.h>
+#include <math.h>
 using namespace std;
 int main()
 {
@@ -7,30 +9,39 @@ int main()
 	char op;
 	while (true) {
 		cout << "Dynamite Calculator \n";
-		cout << "v1.0.1 \n";
+		cout << "v1.2 \n";
 		cin >> a >> op >> b;
+
 		while (cin.fail()) {
 			cin.clear();
 			cin.ignore(100, '\n');
+		}
 
+		{
+			switch (op)
 			{
-				switch (op)
-				{
-				case '+':
-					cout << a + b << endl;
-					break;
-				case '-':
-					cout << a - b << endl;
-					break;
-				case '*':
-					cout << a * b << endl;
-					break;
-				case '/':
-					cout << a / b << endl;
-					break;
-				default:
-					break;
-				}
+			case '+':
+				cout << a + b << endl;
+				break;
+			case '-':
+				cout << a - b << endl;
+				break;
+			case '*':
+				cout << a * b << endl;
+				break;
+			case '/':
+				cout << a / b << endl;
+				break;
+			case '^':
+				cout << pow(a, b) << endl;
+				break;
+			case '`':
+				cout << sqrt(a) << endl;
+				break;
+			case '#':
+				cout << "01001100 01001111 01001100";
+			default:
+				break;
 			}
 		}
 	}
